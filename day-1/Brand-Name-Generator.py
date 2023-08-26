fd0c2e3b78d4c026fca000d304a2f182e894278e
vosdoox <118897820+vosdoox@users.noreply.github.com>
2023-08-26 11:00:29 +0200

    Update and rename Brand-Name-Generator.md to Brand-Name-Generator.py

--- day-1/Brand-Name-Generator.py
+++ day-1/Brand-Name-Generator.py
@@ -1,18 +1,18 @@
 # Brand name generator
 
-  // TODO 1: Create a greeting:
+# TODO 1: Create a greeting:
   
 print("Welcome to the Band Name Generator")
   
-  // TODO 2: Asking the user for the city that they grew up in:
+# TODO 2: Asking the user for the city that they grew up in:
   
 city = input("What's name of the city you grew up in?\n")
   
-  // TODO 3: Asking the user for the name of a pet:
+# TODO 3: Asking the user for the name of a pet:
   
 pet = input("What's the name of your pet?\n")
   
-  // TODO 4: Combining the name of their city and pet and show them their band name:
+# TODO 4: Combining the name of their city and pet and show them their band name:
 
 brand_name = city + " " + pet
 print(brand_name)
