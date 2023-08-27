5386e698902f54de3582b6be24a1c9a7be09c277
Maria <118897820+vosdoox@users.noreply.github.com>
2023-08-27 11:49:00 +0200

    Update and rename Pizza-Delivery to Pizza-Delivery.py

---
+++ day-3/Pizza-Delivery.py
@@ -0,0 +1,38 @@
+# Pizza delivery service 
+
+# TODO 1: Greet the customer:
+print("Welcome to Python Pizza Deliveries!")
+
+# TODO 2: Propose the available sizes of the pizza:
+size = input("What size pizza do you want? S, M, or L ")
+
+# TODO 3: Ask if a customer wants peperoni:
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+
+# TODO 4: Ask if a customer wants extra cheese:
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+# TODO 5: Customize the pizza according to customer's choices and calculate the final price:
+if size == "S":
+  bill = 15
+  if add_pepperoni == "Y":
+    bill += 3
+  elif extra_cheese == "Y":
+    bill += 1
+  print(f"Your final bill is ${bill}.")
+elif size == "M":
+  bill = 20
+  if add_pepperoni == "Y":
+    bill += 3
+  elif extra_cheese == "Y":
+    bill += 1
+  print(f"Your final bill is ${bill}.")
+elif size == "L":
+  bill = 25
+  if add_pepperoni == "Y":
+    bill += 3
+  elif extra_cheese == "Y":
+    bill += 1
+
+# TODO 6: Output the final bill:
+  print(f"Your final bill is ${bill}.")
